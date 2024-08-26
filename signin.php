9047e4c8e9859886b4671b6b4136d15e15e0f037
jainhardik13 <jainhardik130@gmail.com>
2024-08-26 11:21:43 +0530

    commits done php done

---
+++ signin.php
@@ -0,0 +1,33 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "MEDINET";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+
+    $sql = "SELECT * FROM users WHERE username='$user'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $row = $result->fetch_assoc();
+        if (password_verify($pass, $row['password'])) {
+            echo "<script>alert('Sign-in successful!'); window.location.href='profile.php';</script>";
+        } else {
+            echo "<script>alert('Incorrect password.'); window.location.href='login.html';</script>";
+        }
+    } else {
+        echo "<script>alert('User not found.'); window.location.href='login.html';</script>";
+    }
+
+    $conn->close();
+}
+?>
---
+++ signup.php
@@ -0,0 +1,28 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "MEDINET";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user = $_POST['username'];
+    $email = $_POST['email'];
+    $pass = password_hash($_POST['password'], PASSWORD_DEFAULT);
+
+    $sql = "INSERT INTO users (username, email, password) VALUES ('$user', '$email', '$pass')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "<script>alert('Sign up successful!'); window.location.href='login.html';</script>";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
